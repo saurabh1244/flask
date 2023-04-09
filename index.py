77775998736927eb88fc07f1022d49929dc37f41
saurabh1244 <126794525+saurabh1244@users.noreply.github.com>
2023-04-09 06:52:18 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,23 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html')
 
-if __name__ == '__main__':
+@app.route('/login')
+def about():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+# @app.route('/login_info', methods=['POST'])
+# def login_info():
+#     email = request.form.get('email')
+#     password = request.form.get('password')
+#     return "the email is {} and th password is {}" .format(email,password)
+
+if __name__=="__main__":
     app.run(debug=True)
