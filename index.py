44defb44e7fd50da1df40709d4bba11208acfbb3
saurabh1244 <126794525+saurabh1244@users.noreply.github.com>
2023-04-14 01:12:56 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,23 +1,53 @@
-from flask import Flask, render_template, request, url_for
+# from flask import Flask, render_template, request, url_for
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template('home.html')
+
+# @app.route('/login')
+# def about():
+#     return render_template('login.html')
+
+# @app.route('/signup')
+# def signup():
+#     return render_template('signup.html')
+
+# # @app.route('/login_info', methods=['POST'])
+# # def login_info():
+# #     email = request.form.get('email')
+# #     password = request.form.get('password')
+# #     return "the email is {} and th password is {}" .format(email,password)
+
+# if __name__=="__main__":
+#     app.run(debug=True)
+
+
+from flask import Flask , request , jsonify
+import pickle
+import numpy as np
+model = pickle.load(open('model.pk1','rb'))
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return "hello"
 
-@app.route('/login')
-def about():
-    return render_template('login.html')
+@app.route('/predict' ,methods=['POST'])
+def predict():
+    cgpa = request.form.get('cgpa')
+    iq = request.form.get('iq')
+    profile_score = request.form.get('profile_score')
 
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
+    input_query = np.array([[cgpa,iq,profile_score]])
 
-# @app.route('/login_info', methods=['POST'])
-# def login_info():
-#     email = request.form.get('email')
-#     password = request.form.get('password')
-#     return "the email is {} and th password is {}" .format(email,password)
+    result = model.predict(input_query)[0]
 
-if __name__=="__main__":
+    return jsonify({'placement':str(result)})
+
+if __name__ == '__main__':
     app.run(debug=True)
+
+
